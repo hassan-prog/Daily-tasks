355836bca55ef5a97385120b88438e85806e7c80
Hassan El Sayed <55570333+hassan-prog@users.noreply.github.com>
2024-10-27 23:49:00 +0300

    added merge sort

--- Day03.cpp
+++ Day03.cpp
@@ -8,20 +8,16 @@ struct Node {
 	string name;
 	Node* next;
 	Node* prev;
-
-	void PrintNode() {
-		cout << "Name : " << name << "| ID : " << id << '\n';
-	}
-
 };
 
 struct LinkedList {
 	Node* first = NULL;
 	Node* lastInserted = NULL;
-	int id = 1;
+	//int id = 1;
+	int size = 0;
 
 	void PushBack(Node* head) {
-		if (first == nullptr) {
+		if (first == NULL) {
 			first = head;
 			lastInserted = head;
 		}
@@ -32,7 +28,8 @@ struct LinkedList {
 		}
 
 		head->next = nullptr;
-		head->id = id++;
+		//head->id = id++;
+		size++;
 	}
 
 	Node* search(int id) {
@@ -71,6 +68,7 @@ struct LinkedList {
 		if (Emp == curr) {
 			Emp = curr->next;
 		}
+		size--;
 	}
 
 
@@ -83,16 +81,69 @@ struct LinkedList {
 			oldNode->next->prev = newNode;
 			newNode->next = oldNode->next;
 		}
-		newNode->id = id++;
+		//newNode->id = id++;
 		oldNode = NULL;
 	}
 
-	void printList(Node* head) {
-		Node* curr = head;
+	void printList() {
+		Node* curr = first;
 		while (curr != NULL) {
 			cout << "Emp name: " << curr->name << ", Emp ID: " << curr->id << '\n';
 			curr = curr->next;
 		}
+	}
+
+	LinkedList* mergeSort() {
+		if (!this || !this->first == NULL || !this->lastInserted == NULL)
+			return new LinkedList(*this);
+
+		int mid = size / 2;
+
+		LinkedList* list1 = new LinkedList();
+		LinkedList* list2 = new LinkedList();
+
+		Node* current = this->first;
+
+		for (int i = 0; i < size; i++) {
+			if (i < mid) {
+				list1->PushBack(new Node(*current));
+			}
+			else {
+				list2->PushBack(new Node(*current));
+			}
+			current = current->next;
+		}
+
+		list1->mergeSort();
+		list2->mergeSort();
+
+		LinkedList* newList = new LinkedList();
+
+		Node* list1Current = list1->first;
+		Node* list2Current = list2->first;
+
+		while (list1Current != NULL && list2Current != NULL) {
+			if (list1Current->id <= list2Current->id) {
+				newList->PushBack(new Node(*list1Current));
+				list1Current = list1Current->next;
+			}
+			else {
+				newList->PushBack(new Node(*list2Current));
+				list2Current = list2Current->next;
+			}
+		}
+
+		while (list1Current != NULL) {
+			newList->PushBack(new Node(*list1Current));
+			list1Current = list1Current->next;
+		}
+		while (list2Current != NULL) {
+			newList->PushBack(new Node(*list2Current));
+			list2Current = list2Current->next;
+		}
+		delete list1Current;
+		delete list2Current;
+		return newList;
 	}
 
 };
@@ -105,24 +156,53 @@ int main()
 	Node secondNode;
 	Node thirdNode;
 	Node forthNode;
+	Node fifthNode;
+	Node sixthNode;
+	Node seventhNode;
+
 	firstNode.name = "Alaa";
-
+	firstNode.id = 1;
 	secondNode.name = "Fadl";
-
+	secondNode.id = 2;
 	thirdNode.name = "Ahmed";
-
-	forthNode.name = "Merna";
+	thirdNode.id = 3;
+	forthNode.name = "Foad";
+	forthNode.id = 6;
+	fifthNode.name = "Fady";
+	fifthNode.id = 11;
+	sixthNode.name = "Ali";
+	sixthNode.id = 4;
+	seventhNode.name = "Mohamed";
+	seventhNode.id = 9;
 
 	Employees.PushBack(&firstNode);
 	Employees.PushBack(&secondNode);
 	Employees.PushBack(&thirdNode);
-	Employees.printList(&firstNode);
+	Employees.printList();
+	cout << "Push back test\n\n";
 
-	Employees.deleteEmp(&secondNode);
-	Employees.printList(&firstNode);
 
 	Employees.replaceEmp(&secondNode, &forthNode);
-	Employees.printList(&firstNode);
+	Employees.printList();
+	cout << "replace test\n\n";
 
-	//cout << Employees.search(3)->name << "this is my node";
+	Employees.deleteEmp(&forthNode);
+	Employees.printList();
+	cout << "delete test\n\n";
+
+	Employees.PushBack(&fifthNode);
+	Employees.PushBack(&sixthNode);
+	Employees.PushBack(&seventhNode);
+
+	//Employees.deleteEmp(&sixthNode);
+	Employees.printList();
+	cout << "delete and push back\n\n";
+
+	Employees.mergeSort();
+	Employees.printList();
+	cout << "Merge sort test\n\n";
+
+	cout << "Emp with id 3: " << Employees.search(3)->name << '\n';
+
+
 }
