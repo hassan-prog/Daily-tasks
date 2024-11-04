77db966ad79cb705d0d441c26b61d6ba05f9723f
Hassan El Sayed <55570333+hassan-prog@users.noreply.github.com>
2024-11-04 09:19:45 +0200

    testing

--- Snake-SFML.cpp
+++ Snake-SFML.cpp
@@ -1,24 +1,17 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    return 0;
+void main(int argc, char** argv[]) {
+	sf::RenderWindow window(sf::VideoMode(1080, 720), "Snake");
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				// Close window button clicked.
+				window.close();
+			}
+		}
+		window.clear(sf::Color::Black);
+		// Draw here.
+		window.display();
+	}
 }
