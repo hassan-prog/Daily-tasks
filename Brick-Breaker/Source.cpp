c48db320a20d29df5babc7aee422efcdff098ba8
Hassan El Sayed <55570333+hassan-prog@users.noreply.github.com>
2024-12-13 19:56:22 +0200

    first commit of Brick Breaker

---
+++ Brick-Breaker/Source.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <gl/glew/glew.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+
+using namespace std;
+
+const GLint WIDTH = 600, HEIGHT = 600;
+
+int Init()
+{
+	GLenum err = glewInit();
+	if (err != GLEW_OK)
+	{
+		cout << "Error";
+		getchar();
+		return 1;
+	}
+	else
+	{
+		if (GLEW_VERSION_3_0)
+			cout << "Driver support OpenGL 3.0\nDetails:\n";
+	}
+	cout << "\tUsing glew " << glewGetString(GLEW_VERSION) << endl;
+	cout << "\tVendor: " << glGetString(GL_VENDOR) << endl;
+	cout << "\tRenderer: " << glGetString(GL_RENDERER) << endl;
+	cout << "\tVersion: " << glGetString(GL_VERSION) << endl;
+	cout << "\tGLSL:" << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+
+	glClearColor(1, 0, 0, 1);
+
+	return 0;
+}
+
+void Render()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void Update()
+{
+	// add all tick code
+}
+
+int main()
+{
+	sf::ContextSettings context;
+	context.depthBits = 24;
+	sf::Window window(sf::VideoMode(WIDTH, HEIGHT), "SFML works!", sf::Style::Close, context);
+
+	if (Init()) return 1;
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+
+			case sf::Event::Closed:
+				window.close();
+				break;
+			}
+		}
+
+		Update();
+		Render();
+
+		window.display();
+	}
+	return 0;
+}
--- DrawLine/FS2.glsl
+++
@@ -1,8 +0,0 @@
-#version 330
-
-in vec4 frag_color;
-
-void main()
-{
-	gl_FragColor = frag_color;
-}
