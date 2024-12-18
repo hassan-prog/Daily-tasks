ed8a9cf7c4c6e6c6a9bcc32c60f7f57c3c18a656
Hassan El Sayed <55570333+hassan-prog@users.noreply.github.com>
2024-12-18 13:50:45 +0200

    Employee done

---
+++ C#_Course/Day01/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Day01
+{
+    enum Gender
+    {
+        Male,
+        Female
+    }
+    internal struct Employee
+    {
+        private static int currentID = 0;
+        private int id;
+        private float salary;
+        private HiringDate hiringDate;
+        private Gender gender;
+        private string genderText;
+
+        public Employee(float _salary, HiringDate _hiringDate, Gender _gender)
+        {
+            id = ++currentID;
+            salary = _salary;
+            hiringDate = _hiringDate;
+            gender = _gender;
+            genderText = (gender == Gender.Male) ? "Male" : "Female";
+        }
+
+        public int getID() { return id; }
+        public float getSalary() { return salary; }
+        public void setSalary(float _salary) { salary = _salary; }
+        public HiringDate getHiringDate() { return hiringDate; }
+        public void setHiringDate(HiringDate _hiringDate) { hiringDate = _hiringDate; }
+        public Gender getGender() { return gender; }
+        public void setGender(Gender _gender)
+        {
+            gender = _gender;
+            genderText = (gender == Gender.Male) ? "Male" : "Female";
+
+        }
+
+        public void printEmpDetails()
+        {
+            Console.WriteLine($"ID: {id},Salary: {salary}, Hiring Date: {hiringDate.getDay()}/{hiringDate.getMonth()}/{hiringDate.getYear()}" +
+                $"Gender: {genderText}");
+        }
+
+    }
+}
---
+++ C#_Course/Day01/HiringDate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Day01
+{
+    internal struct HiringDate
+    {
+        private int day;
+        private int month;
+        private int year;
+
+        public HiringDate()
+        {
+            day = 0;
+            month = 0;
+            year = 0;
+        }
+
+        // getters and setters
+        public int getDay() { return day; }
+        public void setDay(int _day) { day = _day; }
+        public int getMonth() { return month; }
+        public void setMonth(int _month) { month = _month; }
+        public int getYear() { return year; }
+        public void setYear(int _year) { year = _year; }
+    }
+}
