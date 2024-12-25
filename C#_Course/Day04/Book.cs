ad8a473d16dcf2408befd9810d0d58ad73b94534
Hassan El Sayed <55570333+hassan-prog@users.noreply.github.com>
2024-12-25 14:14:49 +0200

    Added new Day04 project & Book class

---
+++ C#_Course/Day04/Book.cs
@@ -0,0 +1,23 @@
+﻿namespace Day04
+{
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+        public Book(string _ISBN, string _Title, string[] _Authors, DateTime _PublicationDate, decimal _Price)
+        {
+            ISBN = _ISBN;
+            Title = _Title;
+            Authors = _Authors;
+            PublicationDate = _PublicationDate;
+            Price = _Price;
+        }
+        public override string ToString()
+        {
+            return $"Book Title: {Title}, Authors: {Authors}, Price: {Price}, Published on: {PublicationDate.ToString()}";
+        }
+    }
+}
