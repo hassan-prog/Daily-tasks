210863396c196b29f5515ddde7328946186a79ea
Hassan El Sayed <55570333+hassan-prog@users.noreply.github.com>
2024-12-28 21:03:38 +0200

    Club and Department classes done

--- C#_Course/Day05/Club.cs
+++ C#_Course/Day05/Club.cs
@@ -2,5 +2,27 @@
 {
     internal class Club
     {
+        public int ClubID { get; set; }
+        public string ClubName { get; set; }
+        private List<Employee> Members;
+        public void AddMember(Employee E)
+        {
+            Members.Add(E);
+            E.EmployeeLayOff += RemoveMember;
+        }
+        ///CallBackMethod
+        public void RemoveMember(object sender, EmployeeLayOffEventArgs e)
+        {
+            Employee? employee = sender as Employee;
+            if (employee != null && e.Cause == LayOffCause.VacationStockNegative)
+            {
+                Members.Remove(employee);
+                Console.WriteLine($"Employee {employee.EmployeeID} removed from Club due to {e.Cause}");
+            }
+            else if (employee != null && e.Cause == LayOffCause.AgeLimitExceeded)
+            {
+                Console.WriteLine($"Employee {employee.EmployeeID} is still a member of Club despite being older than 60");
+            }
+        }
     }
 }
--- C#_Course/Day05/Department.cs
+++ C#_Course/Day05/Department.cs
@@ -2,5 +2,23 @@
 {
     internal class Department
     {
+        public int DeptID { get; set; }
+        public string DeptName { get; set; }
+        private List<Employee> Staff;
+        public void AddStaff(Employee E)
+        {
+            Staff.Add(E);
+            E.EmployeeLayOff += RemoveStaff;
+        }
+        ///CallBackMethod
+        public void RemoveStaff(object sender, EmployeeLayOffEventArgs e)
+        {
+            Employee? employee = sender as Employee;
+            if (employee != null && (e.Cause == LayOffCause.VacationStockNegative || e.Cause == LayOffCause.AgeLimitExceeded))
+            {
+                Staff.Remove(employee);
+                Console.WriteLine($"Employee {employee.EmployeeID} removed from Department due to {e.Cause}");
+            }
+        }
     }
 }
